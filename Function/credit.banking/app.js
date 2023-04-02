dd86f07e0f960239f8d351bfd159bd1ad6d6dc18
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-04-03 00:26:12 +0400

    Update app.js

--- Function/credit.banking/app.js
+++ Function/credit.banking/app.js
@@ -10,3 +10,28 @@ function Crd(){
     document.getElementById('monthly').innerHTML = `Mounthly Payment : ${monthly} AZN`
     document.getElementById('sum').innerHTML = `Loan Amount : ${total} AZN`
 }
+
+
+function calculateResults(){
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+    const principle = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principle * x * calculatedInterest) / (x - 1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+        document.querySelector('#results').style.display = 'block';
+        document.querySelector('#loading').style.display = 'none';
+    } else {
+        showError('Please check your inputs');
+    }
+}
