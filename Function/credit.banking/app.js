3d6b1486be8d2c4a94bd4ff789b9d8b68d6ab2e6
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-04-04 16:04:21 +0400

    Update app.js

--- Function/credit.banking/app.js
+++ Function/credit.banking/app.js
@@ -1,23 +1,11 @@
-function calculateResults(){
-    const amount = document.getElementById('amount');
-    const interest = document.getElementById('interest');
-    const years = document.getElementById('years');
-    const monthlyPayment = document.getElementById('monthly-payment');
-    const totalPayment = document.getElementById('total-payment');
-    const totalInterest = document.getElementById('total-interest');
-    const principle = parseFloat(amount.value);
-    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-    const calculatedPayments = parseFloat(years.value) * 12;
-    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principle * x * calculatedInterest) / (x - 1);
 
-    if(isFinite(monthly)){
-        monthlyPayment.value = monthly.toFixed(2);
-        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
-        document.querySelector('#results').style.display = 'block';
-        document.querySelector('#loading').style.display = 'none';
-    } else {
-        showError('Please check your inputs');
-    }
+function Calculate() {
+    const amount = document.querySelector("#amount").value;
+    const rate = document.querySelector("#rate").value;
+    const months = document.querySelector("#months").value;
+    const interest = (amount * (rate * 0.01)) / months;
+    const total = ((amount / months) + interest).toFixed(2);
+  
+    document.querySelector("#total")
+    .innerHTML = "AZN : (₼)" + total;
 }
