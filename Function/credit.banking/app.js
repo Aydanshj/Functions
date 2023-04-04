3fb56ce0e8d028d9e1c80fbf968ead4aeb81fd3e
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-04-04 14:33:34 +0400

    Update app.js

--- Function/credit.banking/app.js
+++ Function/credit.banking/app.js
@@ -1,17 +1,3 @@
-var total = 0;
-var monthly = 0;
-function Crd(){
-    var amount =parseFloat(document.getElementById('AZN').value)
-    var month = Number(document.getElementById('month').value)
-    var percent = parseFloat(document.getElementById('percentage').value)
-    var percentAdd = (amount * percent)/100 ;
-    var total = amount + percentAdd;
-    var monthly = Math.round(total/month)
-    document.getElementById('monthly').innerHTML = `Mounthly Payment : ${monthly} AZN`
-    document.getElementById('sum').innerHTML = `Loan Amount : ${total} AZN`
-}
-
-
 function calculateResults(){
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
